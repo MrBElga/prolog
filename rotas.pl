4390be65a712fcd092fa75cf7efb85a9127ba7e4
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2024-09-04 19:39:14 -0300

    Update rotas.pl

--- rotas.pl
+++ rotas.pl
@@ -1,19 +1,5 @@
-busca([Elem|_], 1, Elem).
-busca([_|Cauda], Pos, Elem):- busca(Cauda,PAux, Elem),
 
-concatena([],L2,L2).
-concatena([C|Cauda],L2,[C|LAux]):- concatena(Cauda,L2,LAux).
-
-remove([Elem|Cauda], Elem, Cauda).
-remove([C|Cauda],Elem, [C|LAux] ):- remove(Cauda, Elem, LAux).
-
-removetodos([],_,[]).
-removetodos([Elem|Cauda], Elem, LAux):-removetodos(Cauda,Elem,LAux).
-removetodos([C|Cauda],Elem, [C|LAux] ):- removetodos(Cauda, Elem, LAux).
-
-insere(L,Elem,NL) :- remove(NL, Elem, L).
-
-%rotas
+% rotas posssiveis
 liga(pprudente,marilia,179).
 liga(pprudente,assis,127).
 liga(marilia,bauru,103).
@@ -28,7 +14,9 @@ liga(saocarlos,campinas,141).
 liga(campinas,spaulo,98).
 liga(spaulo,santos,71).
 
-pode_ir(X,Y) :- liga(X,Y) ; liga(Y,X).
+pode_ir(X, Y, Dist) :- liga(X, Y, Dist). % rgra de ligação se pode ou n ir
+pode_ir(X, Y, Dist) :- liga(Y, X, Dist).
+
 
 busca(X,Y) :- liga(X,Y).
 busca(X,Y) :- liga(X,Z), busca(Z,Y).
@@ -36,11 +24,29 @@ busca(X,Y) :- liga(X,Z), busca(Z,Y).
 busca2(X, Y, [X,Y]):- liga(X,Y).
 busca2(X, Y, Cam) :- liga(X,Z), busca2(Z,Y,CAux), Cam = [X|CAux].
 
-dfs(Origem, Destino, Cam) :-  rota(Origem, Destino, [Origem], Cam).
+dfs(X, Y, Cam, DistTot) :- 
+    rota(X, Y, [X], Cam, DistTot).
 
-rota(X,X,Cam,Cam).
-rota(Origem, Destino, Visitados, Cam):-
-    pode_ir(Origem, V),
-    not(member(V,Visitados)),
-    rota(V,Destino,[V|Visitados],Cam).
 
+rota(X, X, Cam, Cam, 0).
+rota(X, Y, Visitados, Cam, DistTot) :-
+    pode_ir(X, Z, Dist),
+    not(member(Z, Visitados)),
+    rota(Z, Y, [Z|Visitados], Cam, DistAux),
+    DistTot is Dist + DistAux.
+
+menor_rota(X, Y, MelhorCam, MenorDist) :-
+    findall([Cam, Dist], dfs(X, Y, Cam, Dist), Rotas), %o findall para pegar tds da list
+    menor_distancia(Rotas, [MelhorCam, MenorDist]).
+
+menor_distancia([Rota], Rota). % achando o q tem a mnr rota
+menor_distancia([[Cam, Dist] | Restantes], [MelhorCam, MenorDist]) :-
+    menor_distancia(Restantes, [CamAux, DistAux]),
+    (Dist < DistAux -> (MelhorCam = Cam, MenorDist = Dist) ;
+    (MelhorCam = CamAux, MenorDist = DistAux)).
+
+%consultas feitas
+%menor_rota(pprudente, santos, MelhorCam, MenorDist).
+%%menor_rota(pprudente, spaulo, MelhorCam, MenorDist).
+%menor_rota(pprudente, marilia, MelhorCam, MenorDist).
+%findall([Cam, Dist], dfs(pprudente, spaulo, Cam, Dist), Rotas).
